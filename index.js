1aac5bfcbae98e0a55d958e328aca0cbedeeb7f2
adrianvilla1504 <adriancvilla@gmail.com>
2022-11-28 22:20:14 -0500

    feat: add base of API

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
 require('dotenv').config();
-
 const cors = require('cors');
 const express = require('express');
+const morgan = require('morgan');
+const routes = require('./routes');
+
+const PORT = process.env.PORT || 1234;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
+
+app.listen(PORT, async () => {
+
+  routes(app);
+
+  console.log(`This server is runnig on http://localhost:${PORT}`);
+
+})
---
+++ routes.js
@@ -0,0 +1,9 @@
+const empleado = require('./api/empleado/index');
+const departamento = require('./api/departamento/index');
+
+function routes(app) {
+  app.use('/api/empleado', empleado);
+  app.use('/api/departamento', departamento);
+}
+
+module.exports = routes;
